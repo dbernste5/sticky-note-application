8cb40d8f038639a4ca5e289318be5a006710d942
dbernste5 <dbernste5@student.touro.edu>
2019-06-04 14:02:48 -0400

    delete stickies in progress

--- src/main/java/stickies/StickyController.java
+++ src/main/java/stickies/StickyController.java
@@ -1,5 +1,6 @@
 package stickies;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,10 +52,29 @@ public class StickyController {
 	}
 
 	@ResponseBody
+	@RequestMapping(path = "/deleteStickies", method = RequestMethod.POST)
+	public void deleteStickies(@RequestParam List<String> stickies, HttpServletResponse response) {
+		int count=-1;
+		log.info("stickies to delete in spring: "+stickies);
+		for(String sticky : stickies) {
+				String query = "delete from basicStickies where StickyId=?";
+				count=jdbcTemplate.update(query, sticky);
+		}
+		log.info("in delete stickies: count= "+count);
+		if(count==-1) {
+			//stickies is null
+			response.setStatus(HttpStatus.UNAUTHORIZED.value()); // 401
+		}
+		else {
+			response.setStatus(HttpStatus.OK.value()); // 200
+		}
+	}
+	
+	@ResponseBody
 	@RequestMapping(path = "/userStickies")
 	public List<Map<String, Object>> getAllStickies(@CookieValue("sessionId") String sessionId, HttpServletResponse response) {
 		int userID = LoginController.sessions.get(sessionId);
-		String query = "SELECT Title, Body from basicStickies where UserID=? order by StickyId";
+		String query = "SELECT Title, Body, StickyId from basicStickies where UserID=? order by StickyId";
 		List<Map<String, Object>> stickies = jdbcTemplate.queryForList(query, userID);
 
 		if (stickies.size() > 0) {
